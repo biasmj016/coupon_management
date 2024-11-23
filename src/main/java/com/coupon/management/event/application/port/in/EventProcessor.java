0fff99e983889634ae9615e52867a3cadc9fe5fc
biasmj016 <mjkim031@gmail.com>
2024-11-23 21:16:51 +0900

    feat: EventProcessor 유효성체크 기능 제거

--- src/main/java/com/coupon/management/event/application/port/in/EventProcessor.java
+++ src/main/java/com/coupon/management/event/application/port/in/EventProcessor.java
@@ -1,7 +1,6 @@
 package com.coupon.management.event.application.port.in;
 
 import com.coupon.management.event.application.port.out.EventRepository;
-import com.coupon.management.event.domain.Event;
 import com.coupon.management.event.infrastructure.out.kafka.CouponIssueProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,19 +22,9 @@ public interface EventProcessor {
 
         @Override
         public void process(Long eventID, String memberID) {
-            validate(eventID);
+            logger.info("Issuing coupon for eventID: {} and memberID: {}", eventID, memberID);
             eventRepository.increaseCount(eventID);
             producer.sendIssueEvent(eventID, memberID);
-        }
-
-        private void validate(Long eventID) {
-            int issuedCount = eventRepository.getIssuedCount(eventID);
-            Event event = eventRepository.findById(eventID);
-            logger.info("Issued count: {}, event max Issued Coupons : {}", issuedCount, event.maxIssuedCoupons());
-            if (!event.isIssuable(issuedCount)) {
-                logger.error("All coupons have been issued. Issuance of coupons for this event ({}) is now discontinued.", eventID);
-                throw new IllegalArgumentException("All coupons have been issued.");
-            }
         }
     }
 }
