a2f89c61d208a076916862aaa634a29606b3465b
biasmj016 <mjkim031@gmail.com>
2024-11-23 21:17:11 +0900

    feat: 로그 추가

--- src/main/java/com/coupon/management/coupon/application/port/in/CouponIssuer.java
+++ src/main/java/com/coupon/management/coupon/application/port/in/CouponIssuer.java
@@ -4,6 +4,8 @@ import com.coupon.management.coupon.application.port.out.CouponRepository;
 import com.coupon.management.coupon.domain.Coupon;
 import com.coupon.management.event.application.port.out.EventRepository;
 import com.coupon.management.event.domain.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +14,7 @@ public interface CouponIssuer {
 
     @Service
     class CouponIssuerUseCase implements CouponIssuer {
-
+        private static final Logger logger = LoggerFactory.getLogger(CouponIssuer.class);
         private final EventRepository eventRepository;
         private final CouponRepository couponRepository;
 
@@ -24,6 +26,7 @@ public interface CouponIssuer {
         @Override
         @Transactional
         public Coupon issue(Long eventID, String memberID) {
+            logger.info("Processing request for eventID: {} and memberID: {}", eventID, memberID);
             Event event = eventRepository.findById(eventID);
             return couponRepository.save(new Coupon(event, memberID));
         }
